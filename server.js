b10570b1978bcb272fe3762ad24aaaf037382650
AlejoSRodriguez <alejosrodriguez96@gmail.com>
2022-05-11 13:45:18 -0300

    server update

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use('/api', (req, res) => res.send({"hola": "chau"}))
+
 const PORT = process.env.PORT || 8080
 
 app.listen(PORT, () => console.log('SERVER UP'))
