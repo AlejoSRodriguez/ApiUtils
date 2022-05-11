1784f4973c70e91aaf13f0ac3d02fd70617d1418
AlejoSRodriguez <alejosrodriguez96@gmail.com>
2022-05-11 13:35:29 -0300

    commit

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require("express");
 const app = express();
 
-app.listen(8080, () => console.log('SERVER UP'))
+const PORT = process.env.PORT || 8080
+
+app.listen(PORT, () => console.log('SERVER UP'))
